343d253dc8d3772f2683fe0e99edc66d58e8c8d8
sujer-ux <mat123matvei.123@gmail.com>
2023-03-31 07:26:08 +0500

    tabs

--- modules/tabs.js
+++ modules/tabs.js
@@ -1,24 +1,39 @@
 let ul = document.querySelector('.tabs ul');
+let bar = document.querySelector('.bar ul')
 let line = document.querySelector('.line');
-let lies = document.querySelectorAll('.tabs ul li');
 
-console.log(lies);
 
-ul.addEventListener('click', function(e) {
-    let vrk = e.target.tagName != 'LI' || e.target.classList.contains('active');
-    if (vrk) {
-        return
-    }
-    let offsX = e.target.offsetLeft,
-        offsW = e.target.offsetWidth;
-    console.log(`left: ${offsX}, with: ${offsW}`);
-    line.style.width = `${offsW - 14}px`;
-    line.style.left = `${offsX + 8}px`;
-    let i = 0;
-    while (i < lies.length) {
-        lies[i].classList.remove('active');
-        i++
-    }
-    e.target.classList.add('active');
-    console.log(e.target.tagName);
-});
+function tabSwither(elem, className, line) {
+    elem.addEventListener('click', function(e) {
+        
+        let vrk = e.target.tagName != 'LI' || e.target.classList.contains(className);
+        if (vrk) {
+            return
+        }
+        
+        function lineMove(line) {
+            if (line == undefined) {
+                return
+            }
+            
+            let offsX = e.target.offsetLeft,
+                offsW = e.target.offsetWidth;
+            line.style.width = `${offsW - 14}px`;
+            line.style.left = `${offsX + 8}px`;
+            
+        }
+        lineMove(line);
+        
+        let lis = elem.children;
+        
+        let i = 0;
+        while (i < lis.length) {
+            lis[i].classList.remove(className);
+            i++
+        }
+        e.target.classList.add(className);
+    })
+}
+
+tabSwither(ul, 'active', line);
+tabSwither(bar, 'bActive');
