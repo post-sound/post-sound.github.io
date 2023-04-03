d9f566b6599651c7ca95513c6336f88cf9b15d8d
post-sound <matvey.semushev@icloud.com>
2023-04-04 02:06:43 +0500

    orentation fix

--- modules/tabs.js
+++ modules/tabs.js
@@ -11,29 +11,8 @@ const mediaQuery = window.matchMedia('(max-width: 489px)');
 
 
 function tabSwither(elem, className, line) {
-    window.addEventListener('resize', e => {
-        if (line == undefined) return;
-        let el = document.querySelector('.'+className);
-        
-        if (window.innerWidth <= 489) {
-
-            if (el.classList.contains('min')) return
-            el.classList.add('min');
-            minimize();
-            return
-        }
-
-        if (!el.classList.contains('min')) return;
-        el.classList.remove('min');
-        minimize();
-
-        function minimize() {
-            let offsX = el.offsetLeft,
-                offsW = el.offsetWidth;
-            line.style.width = `${offsW - 24}px`;
-            line.style.left = `${offsX + 12}px`;
-        }
-    })
+    window.addEventListener('resize', resizes);
+    window.addEventListener('deviceorientation', resizes);
     elem.addEventListener('click', function(e) {
         
         let vrk = e.target.tagName != 'LI' || e.target.classList.contains(className);
@@ -58,6 +37,30 @@ function tabSwither(elem, className, line) {
         }
         e.target.classList.add(className);
     })
+    
+    function resizes(e) {
+        if (line == undefined) return;
+        let el = document.querySelector('.'+className);
+        
+        if (window.innerWidth <= 489) {
+
+            if (el.classList.contains('min')) return
+            el.classList.add('min');
+            minimize();
+            return
+        }
+
+        if (!el.classList.contains('min')) return;
+        el.classList.remove('min');
+        minimize();
+
+        function minimize() {
+            let offsX = el.offsetLeft,
+                offsW = el.offsetWidth;
+            line.style.width = `${offsW - 24}px`;
+            line.style.left = `${offsX + 12}px`;
+        }
+    }
 }
 tabSwither(bar, 'l-active');
 tabSwither(ul, 'active', line);
