00468177e9116843c13becad4a5845e2fb106065
post-sound <matvey.semushev@icloud.com>
2023-04-03 06:09:05 +0500

    1

---
+++ sw.js
@@ -0,0 +1,2 @@
+'use strict';
+importScripts('/service_worker/sw-toolbox.js'); toolbox.precache(['../index.html','../styles/main.css']); toolbox.router.get('/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
