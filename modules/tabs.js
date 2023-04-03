6f3b57f23e81bec8d318fc998b6db070372122c6
post-sound <matvey.semushev@icloud.com>
2023-04-04 01:56:14 +0500

    media req

--- modules/tabs.js
+++ modules/tabs.js
@@ -3,24 +3,49 @@ let bar = document.querySelector('.leftMenu ul')
 let line = document.querySelector('.line');
 
 
+const mediaQuery = window.matchMedia('(max-width: 489px)');
+
+
+
+
+
+
 function tabSwither(elem, className, line) {
+    window.addEventListener('resize', e => {
+        if (line == undefined) return;
+        let el = document.querySelector('.'+className);
+        
+        if (window.innerWidth <= 489) {
+
+            if (el.classList.contains('min')) return
+            el.classList.add('min');
+            minimize();
+            return
+        }
+
+        if (!el.classList.contains('min')) return;
+        el.classList.remove('min');
+        minimize();
+
+        function minimize() {
+            let offsX = el.offsetLeft,
+                offsW = el.offsetWidth;
+            line.style.width = `${offsW - 24}px`;
+            line.style.left = `${offsX + 12}px`;
+        }
+    })
     elem.addEventListener('click', function(e) {
         
         let vrk = e.target.tagName != 'LI' || e.target.classList.contains(className);
-        if (vrk) {
-            return
-        }
+        if (vrk) return;
         
         function lineMove(line) {
-            if (line == undefined) {
-                return
-            }
+            if (line == undefined) return;
             
             let offsX = e.target.offsetLeft,
                 offsW = e.target.offsetWidth;
             line.style.width = `${offsW - 24}px`;
             line.style.left = `${offsX + 12}px`;
-            
         }
         lineMove(line);
         
