741df63bb0830f8bef0c8f739c4a080814827eac
sujer-ux <mat123matvei.123@gmail.com>
2023-04-22 04:07:46 +0500

    loader test

--- modules/page.js
+++ modules/page.js
@@ -9,7 +9,9 @@ function onResize() {
         pageItems[i].style.height = `${w - 10}px`;
         i++
     }
-    document.body.classList.remove('hide-page');
+    setTimeout(() => {
+        document.body.classList.remove('hide-page');
+    }, 1300)
     sInit();
 }
 
