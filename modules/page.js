7aaa728f23431a6e73ff9e3a3028c2cdc8723b88
sujer-ux <mat123matvei.123@gmail.com>
2024-12-13 17:19:44 +0500

    tabs-pos

--- modules/page.js
+++ modules/page.js
@@ -1,23 +1,3 @@
-//let page = document.querySelector('.page');
-//
-//let pageItems = page.children;
-//
-//function onResize() {
-//    let i = 0;
-//    while (i < pageItems.length) {
-//        let w = pageItems[i].clientWidth;
-//        pageItems[i].style.height = `${w - 10}px`;
-//        i++
-//    }
-////    setTimeout(() => {
-//        document.body.classList.remove('hide-page');
-////    }, 1300)
-//    sInit();
-//}
-//
-//window.onload = onResize;
-//window.addEventListener('resize', onResize);
-
 getFile("./baser/data/albums.json");
 const page = document.querySelector('.page');
 
